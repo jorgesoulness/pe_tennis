bc8f45221ee4453e51beb9f9920aa2488d8bb3b2
Jorge Malagón <jorge.malagon@roll.mx>
2020-02-01 08:37:41 -0600

    Se agrega maqueta

--- assets/js/scripts/layoutScripts.js
+++ assets/js/scripts/layoutScripts.js
@@ -135,10 +135,19 @@ jQuery(document).ready(function($){
     nextArrow: $('#nextslideHome'),
     responsive: [
       {
-        breakpoint: 1024,
+        breakpoint: 1200,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
           infinite: true,
           dots: true,
         }
